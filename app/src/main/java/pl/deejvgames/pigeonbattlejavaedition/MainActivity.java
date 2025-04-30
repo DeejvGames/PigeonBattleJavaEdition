8145681b7dae33420e457913ae77fc466d4b426f
DeejvGames <deejvgames@gmail.com>
2025-04-30 21:00:51 +0200

    Updated repo files

--- app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/MainActivity.java
+++ app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -11,12 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // GAME IS AT EARLY DEVELOPMENT STAGE. FEATURES MAY BE UNBALANCED!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        setCoinsTextView();
     }
+
+    public static int userCoins = 0;
 
     public void infoButtonEvent(View view){
         AlertDialog.Builder infoButtonDialogBuilder = new AlertDialog.Builder(this);
@@ -44,5 +50,9 @@ public class MainActivity extends AppCompatActivity {
     public void openPigeonsActivity(View view){
         Intent intent = new Intent(MainActivity.this, pigeonsActivity.class);
         startActivity(intent);
+    }
+
+    public void setCoinsTextView(){
+        ((TextView)findViewById(R.id.coinsAmount)).setText(getString(R.string.coins_amount, userCoins));
     }
 }
