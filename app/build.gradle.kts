c1610cd0c1bde24642cc2b4f141e9c0366c63d4a
DeejvGames <deejvgames@gmail.com>
2025-08-12 20:06:35 +0200

    Small UI changes again, changed version to 1.0.0-beta.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.deejvgames.pigeonbattlejavaedition"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.0-beta.2.3"
+        versionCode = 14
+        versionName = "1.0.0-beta.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
