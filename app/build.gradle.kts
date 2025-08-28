4b2debdefab842d801dcfa8f3c1221c2cdd4e8e6
DeejvGames <deejvgames@gmail.com>
2025-08-28 20:49:17 +0200

    Changed version to 1.0.0-beta.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.deejvgames.pigeonbattlejavaedition"
         minSdk = 28
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.0-beta.2.4"
+        versionCode = 15
+        versionName = "1.0.0-beta.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
