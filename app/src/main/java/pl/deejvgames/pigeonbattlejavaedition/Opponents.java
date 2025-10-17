e9c3f130210489cf892d2416b81d2af17cc64c8b
DeejvGames <deejvgames@gmail.com>
2025-10-17 19:56:33 +0200

    Changed Characters' and Opponents' properties

--- app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/Opponents.java
+++ app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/Opponents.java
@@ -2,12 +2,12 @@ package pl.deejvgames.pigeonbattlejavaedition;
 
 public enum Opponents {
     OPPONENT_PIGEON(R.string.default_pigeon, 1200, 50, 0, 0, 0, 0, R.drawable.opponent_pigeon), // NAME, HP, DMG, DMG PER SECOND, TAKES LESS DAMAGE, SPEED BOOST, PRICE
-    OPPONENT_RADIO_PIGEON(R.string.radio_pigeon, 1250, 50, 5, 0, 0, 120, R.drawable.opponent_radio_pigeon),
-    OPPONENT_PIGOBOMB(R.string.pigobomb, 1350, 80, 0, 0, 0, 240, R.drawable.opponent_pigobomb),
-    OPPONENT_FEATHERED_PIGEON(R.string.feathered_pigeon, 1550, 55, 0, 15, 0, 350, R.drawable.opponent_feathered_pigeon),
-    OPPONENT_MILK_PIGEON(R.string.milk_pigeon, 1250, 70, 0, 10, 0, 320, R.drawable.opponent_milk_pigeon),
-    OPPONENT_WHEEL_PIGEON(R.string.wheel_pigeon, 1300, 65, 0, 0, 25, 310, R.drawable.opponent_wheel_pigeon),
-    OPPONENT_NUCLEAR_PIGEON(R.string.nuclear_pigeon, 1450, 90, 10, 0, 0, 400, R.drawable.opponent_nuclear_pigeon);
+    OPPONENT_RADIO_PIGEON(R.string.radio_pigeon, 1250, 50, 5, 0, 0, 200, R.drawable.opponent_radio_pigeon),
+    OPPONENT_PIGOBOMB(R.string.pigobomb, 1350, 60, 0, 0, 0, 450, R.drawable.opponent_pigobomb),
+    OPPONENT_FEATHERED_PIGEON(R.string.feathered_pigeon, 1600, 55, 0, 15, 0, 1400, R.drawable.opponent_feathered_pigeon),
+    OPPONENT_MILK_PIGEON(R.string.milk_pigeon, 1250, 70, 0, 10, 0, 800, R.drawable.opponent_milk_pigeon),
+    OPPONENT_WHEEL_PIGEON(R.string.wheel_pigeon, 1400, 65, 0, 0, 25, 1000, R.drawable.opponent_wheel_pigeon),
+    OPPONENT_NUCLEAR_PIGEON(R.string.nuclear_pigeon, 1450, 90, 10, 0, 0, 1500, R.drawable.opponent_nuclear_pigeon);
 
     private final int characterNameId;
     private final int characterHP;
