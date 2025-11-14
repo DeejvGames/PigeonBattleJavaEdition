1e178746b9722b943f9d59c7f213c15777981962
DeejvGames <deejvgames@gmail.com>
2025-11-14 20:52:01 +0100

    Fixed when opponent should attack player (again)

--- app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/playActivity.java
+++ app/src/main/java/pl/deejvgames/pigeonbattlejavaedition/playActivity.java
@@ -564,7 +564,7 @@ public class playActivity extends AppCompatActivity {
         ImageView player = findViewById(R.id.playerImage);
         attackPlayerThread = new Thread(() -> {
             while(true){
-                if((gameOpponent.getY()==player.getY() || (gameOpponent.getY()<=player.getY()+player.getHeight() && gameOpponent.getY()>=player.getY()-player.getHeight()) || (gameOpponent.getY()>=player.getY() && gameOpponent.getY()<=player.getY()+player.getHeight())) && gameOpponent.getX()>=player.getX()+player.getWidth()){
+                if((gameOpponent.getY()==player.getY() && gameOpponent.getX()>=player.getX()+player.getWidth()) || (gameOpponent.getY()<=player.getY()+player.getHeight() && gameOpponent.getY()>=player.getY()-player.getHeight() && gameOpponent.getX()>=player.getX()+player.getWidth()) || (gameOpponent.getY()>=player.getY() && gameOpponent.getY()<=player.getY()+player.getHeight() && gameOpponent.getX()>=player.getX()+player.getWidth())){
 //                    Log.d("opponentAttack", "Attacking player!");
                     createOpponentDamage();
                 }
